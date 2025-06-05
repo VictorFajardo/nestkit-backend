224460187c19aacadcbf9ee40de97e7af22c243f
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-06-04 20:04:22 -0400

    ✅ implement e2e test cases

--- jest.config.ts
+++ jest.config.ts
@@ -1,28 +1,39 @@
 import type { Config } from 'jest';
-import { compilerOptions } from './tsconfig.json';
-import { pathsToModuleNameMapper } from 'ts-jest';
+
+export const moduleNameMapper = {
+  '^@app/(.*)$': '<rootDir>/src/$1',
+  '^@auth/(.*)$': '<rootDir>/src/auth/$1',
+  '^@users/(.*)$': '<rootDir>/src/users/$1',
+  '^@common/(.*)$': '<rootDir>/src/common/$1',
+  '^@config/(.*)$': '<rootDir>/src/config/$1',
+  '^@prisma-local/(.*)$': '<rootDir>/src/prisma/$1',
+  '^@health/(.*)$': '<rootDir>/src/health/$1',
+  '^@logger/(.*)$': '<rootDir>/src/logger/$1',
+  '^@token/(.*)$': '<rootDir>/src/token/$1',
+  '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  '^@audit-log/(.*)$': '<rootDir>/src/audit-log/$1',
+};
 
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
+  rootDir: '.',
+  testMatch: ['**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  moduleNameMapper: {
-    '^@config/(.*)$': '<rootDir>/config/$1',
-    '^@auth/(.*)$': '<rootDir>/auth/$1',
-    '^@users/(.*)$': '<rootDir>/users/$1',
-    '^@common/(.*)$': '<rootDir>/common/$1',
-    '^@prisma-local/(?!client)(.*)$': '<rootDir>/prisma/$1',
-    '^@token/(.*)$': '<rootDir>/token/$1',
-    '^@utils/(.*)$': '<rootDir>/utils/$1',
-    '^@audit-log/(.*)$': '<rootDir>/audit-log/$1',
-  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/main.ts',
+    '!src/**/*.module.ts',
+    '!src/**/dto/*.ts',
+    '!src/**/interfaces/*.ts',
+    '!src/**/*.spec.ts',
+  ],
+  coverageDirectory: 'coverage',
+  moduleNameMapper: moduleNameMapper,
 };
 
 export default config;
