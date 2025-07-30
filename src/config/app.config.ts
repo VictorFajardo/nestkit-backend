f55b00e28e4b4503b738610614d86b520e03f196
Victor Fajardo <fajardo.de.leon@gmail.com>
2025-07-29 22:05:13 -0400

    😒 update app port

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ import { Env } from './env';
 
 export const AppConfig = {
   env: Env.NODE_ENV,
-  port: Env.PORT,
+  port: Env.PORT || 3000,
   jwtSecret: Env.JWT_SECRET,
   databaseUrl: Env.DATABASE_URL,
 };
